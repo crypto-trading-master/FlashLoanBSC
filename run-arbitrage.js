d15ee857c59ba89e7b7491d9771743d2f8ae33d3
crypto-trading-master <trading_master@outlook.com>
2021-07-13 21:50:43 +0200

    Ankr Websocket working with BSC

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -1,4 +1,14 @@
-const Web3 = require('web3');
-const web3 = Web3(
-    new Web3.providers.WebsocketProvider()
-);
+require("dotenv").config();
+
+const Web3 = require("web3");
+const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ANKR_URL));
+
+web3.eth
+    .subscribe("newBlockHeaders")
+    .on("data", async (block) => {
+        console.log(`New block received. Block # ${block.number}`);
+    })
+    .on("error", error => {
+        console.log(error);
+    });
+
