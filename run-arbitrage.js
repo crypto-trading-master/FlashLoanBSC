16bd511f7f8e478d28d6d712730ecdfef93fdecf
crypto-trading-master <trading_master@outlook.com>
2021-07-16 21:14:19 +0200

    Pancakeswap working

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -2,15 +2,21 @@ require("dotenv").config();
 const { ChainId, Token, TokenAmount, Fetcher } = require('@pancakeswap/sdk');
 const Web3 = require("web3");
 const { mainnet: addresses } = require("./addresses");
-const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ANKR_URL));
+//const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ANKR_URL));
+const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://bsc-ws-node.nariox.org:443'));
 
+const AMOUNT_BUSD_WEI = web3.utils.toBN(web3.utils.toWei('20000'));
+const AMOUNT_BNB_WEI = web3.utils.toBN(web3.utils.toWei('1'));
+
+const {JsonRpcProvider} = require("@ethersproject/providers");
+const provider = new JsonRpcProvider('https://bsc-dataseed.binance.org/');
 
 const init = async () => {
-    const [busd, wbnb] = await Promise.all(
-        [addresses.tokens.busd, addresses.tokens.wbnb].map((tokenAddress) =>
-        Fetcher.fetchTokenData(ChainId.MAINNET, tokenAddress)));        
+
+    const busd = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.busd, provider);
+    const wbnb = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.wbnb, provider);    
     
-    const busdWbnb = await Fetcher.fetchPairData(busd, wbnb);
+    const busdWbnb = await Fetcher.fetchPairData(busd, wbnb, provider);
 
     web3.eth
         .subscribe("newBlockHeaders")
@@ -23,6 +29,8 @@ const init = async () => {
             ]);
 
             console.log(pancakeswapResults);
+            console.log('--> ' + pancakeswapResults[0][0].raw.toString());
+            console.log('--> ' + pancakeswapResults[1][0].raw.toString());
 
         })
         .on("error", error => {
