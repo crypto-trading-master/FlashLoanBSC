db3360b4dd1b8317d526d92b5e8e29eb8d5dd1ea
crypto-trading-master <trading_master@outlook.com>
2021-07-19 21:21:12 +0200

    Checksum BUSD token changed

--- run-arbitrage.js
+++ run-arbitrage.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const { ChainId, Token, TokenAmount, Fetcher } = require('@pancakeswap/sdk');
 const Web3 = require("web3");
 const { mainnet: addresses } = require("./addresses");
-//const web3 = new Web3(new Web3.providers.WebsocketProvider(process.env.ANKR_URL));
+
 const web3 = new Web3(new Web3.providers.WebsocketProvider('wss://bsc-ws-node.nariox.org:443'));
 
 const AMOUNT_BNB = 100;
@@ -13,18 +13,15 @@ const {JsonRpcProvider} = require("@ethersproject/providers");
 const provider = new JsonRpcProvider('https://bsc-dataseed.binance.org/');
 
 const init = async () => {
-
-    const busd = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.busd, provider);
-    const wbnb = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.wbnb, provider);    
-    
-    const busdWbnb = await Fetcher.fetchPairData(busd, wbnb, provider);
-
     web3.eth
         .subscribe("newBlockHeaders")
         .on("data", async (block) => {
             console.log(`New block received. Block # ${block.number}`);
 
-            
+            const busd = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.busd, provider);
+            const wbnb = await Fetcher.fetchTokenData(ChainId.MAINNET, addresses.tokens.wbnb, provider);    
+    
+            const busdWbnb = await Fetcher.fetchPairData(busd, wbnb, provider);
 
             const pancakeswapResults = await Promise.all([
                 busdWbnb.getOutputAmount(new TokenAmount(busd, AMOUNT_BUSD_WEI)),
@@ -37,12 +34,6 @@ const init = async () => {
             };
             console.log("Pancakeswap BNB/BUSD");
             console.log(pancakeswapRates);            
-
-            /* 
-            console.log(pancakeswapResults);
-            console.log('--> ' + pancakeswapResults[0][0].raw.toString());
-            console.log('--> ' + pancakeswapResults[1][0].raw.toString());
-            */
 
         })
         .on("error", error => {
